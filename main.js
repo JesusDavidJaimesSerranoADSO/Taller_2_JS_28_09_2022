228afcf3047f84b1a9b3337bc8dfb080f45fd4cd
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-14 10:22:57 -0500

    Ejercicio 21

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 1. Escribir un programa que sume, reste, multiplique y divida dos números
+
+    // Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    let valorx = Number(prompt(`ingrese el valor del punto x1`,5));
+    let valory = Number(prompt(`ingrese el valor del punto y1`,8));
+    let valorx2 = Number(prompt(`ingrese el valor del punto x2`,3));
+    let valory2 = Number(prompt(`ingrese el valor del punto y2`,9));
+    let sbase = ((valorx2-valorx)**2 + (valory2-valory)**2)
     
-    let num1 = Number(prompt("Ingrese el numero 1", 2));
-    let num2 = Number(prompt("Ingrese el numero 2", 2));
 
-    console.log(`La suma es ${num1} + ${num2} = ${num1 + num2}`);
-    console.log(`La resta es ${num1} - ${num2} = ${num1 - num2}`);
-    console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
-    console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
-
-
-
-
-
-
+    console.log(`la distancia entre dos puntos es de ${sbase}`);
 
 })
