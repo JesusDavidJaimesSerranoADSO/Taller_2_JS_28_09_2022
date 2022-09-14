03b994714951c6f9d1f3107561ead5d1bef45a99
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-14 10:53:33 -0500

    Ejercicio 23

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-//     Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
-// descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
-// de la compra.
-    let valor1 = Number(prompt(`ingrese el valor del punto 1`,5000));
-    let valor2 = Number(prompt(`ingrese el valor del punto 2`,8000));
-    let valor3 = Number(prompt(`ingrese el valor del punto 3`,3000));
-    let valor4 = Number(prompt(`ingrese el valor del punto 4`,9000))
-    let valor5 = Number(prompt(`ingrese el valor del punto 5`,3000));
-    let total = (valor1 - (valor1 * 0,05)) + (valor2 - (valor2 * 0,05)) + (valor3) + (valor4 - (valor4 * 0,02)) + (valor5 - (valor5 * 0,02))
-    
-    console.log(`el valor del 1 producto es de ${valor1 - (valor1 * 0,05)}`);
-    console.log(`el valor del 2 producto es de ${valor2 - (valor2 * 0,05)}`);
-    console.log(`el valor del 3 producto es de ${valor3}`);
-    console.log(`el valor del 4 producto es de ${valor4 - (valor4 * 0,02)}`);
-    console.log(`el valor del 5 producto es de ${valor5 - (valor5 * 0,02)}`);
-    console.log(`el valor total de la compra es de ${total}`);
+    //Generar el algoritmo que dé como resultado el tiempo estimado para el llenado de un depósito.
+    // • Q=V/t, siendo Q (caudal), V (volumen) y t (tiempo).
+    // • Volumen = PI *(radio^2)* H (altura del depósito)
+    // • El tiempo se encuentra en minutos.
+    // • Normalmente se mide el volumen en litros y el tiempo en segundos.
+    let caudal = Number(prompt(`caudal en litros por segundos`,53));
+    let radio = Number(prompt(`ingrese el radio de la mngera`,8));
+    let altura = Number(prompt(`ingrese la altura del tanque`,300));
+    let vmen = Math.PI * (radio ** 2) * altura
+    let res = vmen / caudal
+    console.log(`el tiempo estimado para el llenado de un depósito es de ${res}`);
 
 })
