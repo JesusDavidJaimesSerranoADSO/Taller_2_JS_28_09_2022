05c1366db0d7bf1c5a4a4e10aeef486a338c1170
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-14 11:09:02 -0500

    Ejercicio 25

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-    // Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
-    // $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
-    // sobran $91000. ¿cuánto dinero tenía?
-    let llaves = Number(prompt(`precio de la llave por unidad`,11500));
-    let bomba = Number(prompt(`precio de la bomba`,1168000));
-    let pernos = Number(prompt(`precio del perno por unidad`,87000));
-    alert(`dinero sobrante 91000`)
-    let total = (llaves * 5) + bomba + (pernos * 3) + 91000
+    // En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
+    // de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
+    // nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
+    // hacer el bizcocho es
+    let cacao = Number(prompt(`ingrese la cantidad de cacao para la receta`,20));
+    let total = 100 * (cacao / 10)
     
-    console.log(`el dueño de la obra tenia $${total}`);
+    console.log(`la cantidad de harina que necesitas es ${total} gramos`);
 
 })
